2d12f3c19934e433c9eaf1056830ceb877fce1e9
HERDANTIO <herdantio.sindhunata@binus.ac.id>
2021-11-29 20:44:01 +0700

    setup vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import store from './store/index'
 
 import './styles/index.css'
 
-createApp(App).mount('#app')
+createApp(App).use(store).mount('#app')
---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore } from "vuex";
+import registration from './modules/registration'
+
+const store = createStore({
+    state: {
+    },
+    mutations: {
+    },
+    actions: {
+    },
+    modules: {
+        registration
+    }
+});
+
+export default store;
